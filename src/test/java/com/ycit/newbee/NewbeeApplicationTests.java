a7fea980db2d614ca43ca068d1b32c978343ec58
babeijing <oldhorse.cc@gmail.com>
2022-12-17 16:17:44 +0800

    init project of upload-to-github

---
+++ src/test/java/com/ycit/newbee/NewbeeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ycit.newbee;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NewbeeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
